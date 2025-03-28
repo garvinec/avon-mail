d00521163f24f5c17bd4913efe3cebb3d8dfcd0a
garvinec <chan.gar@northeastern.edu>
2025-03-28 00:56:45 -0400

    adds page tab

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,8 +1,8 @@
 import { cookies } from "next/headers";
-import Image from "next/image";
 
 import { Mail } from "@/components/mail/Mail";
 import { accounts, mails } from "@/public/TestMailData";
+import Header from "@/components/Header";
 
 export default async function Home() {
   const cookieStore = await cookies();
@@ -15,21 +15,8 @@ export default async function Home() {
   return (
     <main className="min-h-screen w-full">
       <>
-        <div className="md:hidden">
-          <Image
-            src="/examples/mail-dark.png"
-            width={1280}
-            height={727}
-            alt="Mail"
-            className="hidden dark:block"
-          />
-          <Image
-            src="/examples/mail-light.png"
-            width={1280}
-            height={727}
-            alt="Mail"
-            className="block dark:hidden"
-          />
+        <div>
+          <Header />
         </div>
         <div className="hidden flex-col md:flex">
           <Mail
